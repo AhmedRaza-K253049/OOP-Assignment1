5d74f7eef8c75f13eac449ba22ba1500cf7fad5a
Muhammad Ahmed Raza <K253049@nu.edu.pk>
2026-02-27 19:05:34 +0500

    Update

--- Assignment1_25K3049.cpp
+++ Assignment1_25K3049.cpp
@@ -1,27 +1,82 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Seller {
-private:
 
-};
-class Buyer{};
-class Car {
-public:
-    string EngineType;
-    int EngineCapacity;
+class Car{
+private:
+    static int ID;
+    string Company;
+    string Name;
+    int EngineCC;
     int Mileage;
     string Color;
-    char Transmission;
-    string RegisteredIn;
-};
-class Coupe: public Car {
+    int Price;
+    int RegisterationYear;
+    Car (Car &obj){}
 
+public:
+    string returnCompany(){
+        return Company;
+    }
+    string returnName(){
+        return Name;
+    }
+    int returnEngineCC(){
+        return EngineCC;
+    }
+    int returnMileage(){
+        return Mileage;
+    }
+    string returnColor(){
+        return Color;
+    }
+    int returnRegisterationYear(){
+        return RegisterationYear;
+    }
+    int returnPrice(){
+        return Price;
+    }
+    void setCompany(string input){
+        Company = input;
+    }
+    void setName(string input){
+        Name = input;
+    }
+    void setEngineCC(int input){
+        EngineCC = input;
+    }
+    void setMileage(int input){
+        Mileage = input;
+    }
+    void setColor(string input){
+        Color = input;
+    }
+    void setRegisterationYear(int input){
+        RegisterationYear = input;
+    }
+    void setPrice(int input){
+        Price = input;
+    }
+    void Display(){
+        cout<< "Company: "<< Company<< endl;
+        cout<< "Name: "<< Name<<endl;
+        cout<< "Engine CC: "<< EngineCC<< endl;
+        cout<< "Mileage: "<< Mileage<< endl;
+        cout<< "Color: "<< Color<< endl;
+        cout<< "Registered In Year: "<< RegisterationYear<< endl;
+        cout<< "Price: "<< Price<< endl;
+    }
 };
-// class Sedan{};
-// class Suv{};
-// class OffRoad{};
-// class Truck{};
-// class Vav{};
-// class Pickup{};
-class Bike{};
+class Seller {
+    private:
+        string Name;
+        string Email;
+        int Number;
+        Car *Selling;
+        int sellingCount;
+    public:
+    Seller(string NameIn, string EmailIn, int NumberIn){
+        
+    }
+};
+class Buyer{};
