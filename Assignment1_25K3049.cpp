4ad877670d206ed370c668257914b022ab9164e1
Muhammad Ahmed Raza <K253049@nu.edu.pk>
2026-02-27 22:37:16 +0500

    Update

--- Assignment1_25K3049.cpp
+++ Assignment1_25K3049.cpp
@@ -2,27 +2,49 @@
 #include <string>
 using namespace std;
 
+class Engine{
+    private:
+        int CC;
+        string FuelType;
+        string TransmissionType;
+        int Cylinder;
+        int Turbo;
+    public:
+        Engine(){};
+        Engine(int CCIn, string FuelIn, string TransmissionIn, int CylinderIn, int TurboIn){
+            CC = CCIn;
+            FuelType = FuelIn;
+            TransmissionType = TransmissionIn;
+            Cylinder = CylinderIn;
+            Turbo = TurboIn;
+        }
+};
 class Car{
 private:
-    static int ID;
+    static int Id;
     string Company;
     string Name;
-    int EngineCC;
+    Engine engine;
     int Mileage;
     string Color;
     int Price;
     int RegisterationYear;
-    Car (Car &obj){}
-
 public:
+    Car(){};
+    Car(string CompanyIn, string NameIn, int MileageIn, string ColorIn, int PriceIn, int RegisterationIn, int CCIn, string FuelIn, string TransmissionIn, int CylinderIn, int TurboIn): engine(CCIn, FuelIn, TransmissionIn, CylinderIn, TurboIn){
+        Id++;
+        Company = CompanyIn;
+        Name = NameIn;
+        Mileage = MileageIn;
+        Color = ColorIn;
+        Price = PriceIn;
+        RegisterationYear = RegisterationIn;
+    }
     string returnCompany(){
         return Company;
     }
     string returnName(){
         return Name;
-    }
-    int returnEngineCC(){
-        return EngineCC;
     }
     int returnMileage(){
         return Mileage;
@@ -42,9 +64,6 @@ public:
     void setName(string input){
         Name = input;
     }
-    void setEngineCC(int input){
-        EngineCC = input;
-    }
     void setMileage(int input){
         Mileage = input;
     }
@@ -58,25 +77,91 @@ public:
         Price = input;
     }
     void Display(){
+        cout<< "Car ID: "<< Id<< endl;
         cout<< "Company: "<< Company<< endl;
         cout<< "Name: "<< Name<<endl;
-        cout<< "Engine CC: "<< EngineCC<< endl;
         cout<< "Mileage: "<< Mileage<< endl;
         cout<< "Color: "<< Color<< endl;
         cout<< "Registered In Year: "<< RegisterationYear<< endl;
         cout<< "Price: "<< Price<< endl;
     }
+    static int returnID(){
+        return ID;
+    }
 };
+
 class Seller {
     private:
         string Name;
-        string Email;
         int Number;
-        Car *Selling;
-        int sellingCount;
+        const string Username;
+        const string Password;
+        Car Selling[5];
+        int SellingCount;
     public:
-    Seller(string NameIn, string EmailIn, int NumberIn){
-        
-    }
+        Seller(string NameIn, string UsernameIn, string PasswordIn, int NumberIn): Username(UsernameIn), Password(PasswordIn){
+            Name = NameIn;
+            Number = NumberIn;
+            SellingCount = 0;
+        }
+        void addCar(string CompanyIn, string NameIn, int MileageIn, string ColorIn, int PriceIn, int RegisterationIn, int CCIn, string FuelIn, string TransmissionIn, int CylinderIn, int TurboIn){
+            Selling[SellingCount] = Car(CompanyIn, NameIn, MileageIn, ColorIn, PriceIn, RegisterationIn, CCIn, FuelIn, TransmissionIn, CylinderIn, TurboIn);
+            SellingCount++;
+        }
+        void removeCar(int IdIn){
+            for(int i = 0; i < SellingCount; i++){
+                if(Selling[i].returnID() == IdIn){
+                    for(int j = i; j < SellingCount; j++){
+                        Selling[j] = Selling[j + 1];
+                    }
+                SellingCount--;
+                }
+            }
+        }
 };
-class Buyer{};
+class Buyer{
+   private:
+        string Name;
+        int Number;
+        const string Username;
+        const string Password;
+        Car **Favourite;
+        int FavouriteMax;
+        int FavouriteCount;
+    public:
+        Buyer(string NameIn, string UsernameIn, string PasswordIn, int NumberIn): Username(UsernameIn), Password(PasswordIn){
+            Name = NameIn;
+            Number = NumberIn;
+            FavouriteCount = 0;
+            FavouriteMax = 5;
+            Favourite = new Car*[5];
+        }
+        void addFavourite(Car *obj){
+            if(FavouriteCount < FavouriteMax){
+                Favourite[FavouriteCount] = obj;
+                FavouriteCount++;
+            }else{
+                Car **temp;
+                temp = new Car*[FavouriteMax * 2];
+                for(int i = 0; i < FavouriteMax; i++){
+                    temp[i] = Favourite[i];
+                }
+                temp[FavouriteCount] = obj;
+                delete[] Favourite;
+                Favourite = temp;
+                FavouriteCount++;
+                FavouriteMax *= 2;
+            }
+        }
+        void removeFavourite(int IdIn){
+            for(int i = 0; i < FavouriteCount; i++){
+                if(Favourite[i]->returnID() == IdIn){
+                    for(int j = i; j < FavouriteCount; j++){
+                        Favourite[j] = Favourite[j + 1];
+                    }
+                FavouriteCount--;
+                }
+            }
+        }
+
+};
