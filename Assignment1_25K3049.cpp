caaaaf420600b1f05483beea2ca3a5007992996f
Muhammad Ahmed Raza <K253049@nu.edu.pk>
2026-02-27 16:29:49 +0500

    Added main file

---
+++ Assignment1_25K3049.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Seller {
+private:
+
+};
+class Buyer{};
+class Car {
+public:
+    string EngineType;
+    int EngineCapacity;
+    int Mileage;
+    string Color;
+    char Transmission;
+    string RegisteredIn;
+};
+class Coupe: public Car {
+
+};
+// class Sedan{};
+// class Suv{};
+// class OffRoad{};
+// class Truck{};
+// class Vav{};
+// class Pickup{};
+class Bike{};
